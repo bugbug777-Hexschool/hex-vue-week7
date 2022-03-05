5ff975eee022def085a2b02b865f9f03f3bcf142
sihle <gubug777@gmail.com>
2022-03-05 08:03:32 +0800

    [add] Add front end view

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    component: () => import('../views/FrontView.vue'),
   },
   {
     path: '/about',
-    name: 'about',
     component: () => import('../views/AboutView.vue'),
   },
 ];
