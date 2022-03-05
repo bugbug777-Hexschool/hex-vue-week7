81dd518d8ae7f9497cd7810f65fa5caca3e5b433
sihle <gubug777@gmail.com>
2022-03-05 08:24:45 +0800

    [add] Add navbar to dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,3 +1,14 @@
 <template>
+  <Navbar />
   <h1>Dashboard</h1>
 </template>
+
+<script>
+import Navbar from '@/components/AdminNavbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
