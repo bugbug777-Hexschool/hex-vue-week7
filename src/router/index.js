4100ef97aedd7108ffb21adbd6ae235989a3ab74
sihle <gubug777@gmail.com>
2022-03-05 08:08:30 +0800

    [add] Add admin route

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ const routes = [
     path: '/login',
     component: () => import('../views/LoginView.vue'),
   },
+  {
+    path: '/admin',
+    component: () => import('../views/DashboardView.vue'),
+  },
 ];
 
 const router = createRouter({
