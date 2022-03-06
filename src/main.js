1ab25d24f6af08291aaea6efa2b38444489dcec5
sihle <gubug777@gmail.com>
2022-03-06 11:42:20 +0800

    [add] Add vee-validate configuration

--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@ import { createApp } from 'vue';
 import 'bootstrap';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import {
+  Field, Form, ErrorMessage,
+  defineRule,
+} from 'vee-validate';
+import { required, email, min } from '@vee-validate/rules';
 import App from './App.vue';
 import router from './router';
 
+// Vee rules
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+
+// Main App
 const app = createApp(App);
+app.component('VForm', Form);
+app.component('VField', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.use(router);
 app.use(VueAxios, axios);
 app.mount('#app');
